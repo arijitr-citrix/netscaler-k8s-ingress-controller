cb0d8585a9c66d7f06287602f84ec71e1cca045a
Arijit Ray <arijit.ray@citrix.com>
2026-08-26 12:12:38 +0000

    Release 4.2.26

--- docs/how-to/hpa/values.sh
+++ docs/how-to/hpa/values.sh
@@ -1,6 +1,6 @@
 VPX_IP="VPX_IP"
 CPX_IMAGE="quay.io/netscaler/netscaler-cpx:14.1-66.59"
-CIC_IMAGE="quay.io/netscaler/netscaler-k8s-ingress-controller:4.1.17"
+CIC_IMAGE="quay.io/netscaler/netscaler-k8s-ingress-controller:4.2.26"
 EXPORTER="quay.io/citrix/citrix-adc-metrics-exporter:1.4.9"
 VPX_PASSWORD="VPX_PASSWORD"
 VPX_VIP="VIRTUAL_IP_VPX"
